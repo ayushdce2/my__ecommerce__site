b3ae13b004ad2ddc2f7f19fea6d62ce1e699d867
Ayush Pratap Singh <ayushdce2@gmail.com>
2026-01-22 21:03:50 +0530

    add

--- frontoffice/src/components/SubComponent/CategorySidebar.jsx
+++ frontoffice/src/components/SubComponent/CategorySidebar.jsx
@@ -16,7 +16,7 @@ const CategorySidebar = () => {
      const { pathname } = useLocation();
 
    if(loading){
-        return (<div className=' h-full bg-gradient-to-b from-blue-400 to-indigo-200 text-gray-200 border-r-gray-950 p-3 flex flex-col gap-5 items-center justify-center'><img src="/images/loading.gif" className='w-[5rem]' /> <p className='font-bold text-3xl text-shadow-2xs'>Loading</p></div>)
+        return (<div className='  bg-gradient-to-b from-blue-400 to-indigo-200 text-gray-200 border-r-gray-950 p-3 flex flex-col gap-5 items-center justify-center'><img src="/images/loading.gif" className='w-[5rem]' /> <p className='font-bold text-3xl text-shadow-2xs'>Loading</p></div>)
     }
     if(allCategories.length <= 0){
       return "No Category Found"
