0726d9ad2800795271f88f077ab662eae63858dc
Ayush Pratap Singh <ayushdce2@gmail.com>
2026-04-15 16:53:08 +0530

    add

--- backoffice/src/App.jsx
+++ backoffice/src/App.jsx
@@ -24,7 +24,7 @@ function App() {
     <>
 
       
-      <BrowserRouter basename="/admin">
+      <BrowserRouter basename="/admin/">
       <RefreshHandler setisAuthenticated={setisAuthenticated}/>
         <Routes>
 
